15396eabb2193390cc7de1fb9ed20c4b21badb90
Nathan Clevenger <nateclev@gmail.com>
2022-09-03 03:05:55 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,24 @@
+export default {
+  fetch: (req, env) => {
+    const { user, redirect, body } = await env.CTX.fetch(req).then(res => res.json())
+    if (redirect) return Response.redirect(redirect)
+    const { origin, pathname, search } = new URL(req.url)
+     
+    return new Response(JSON.stringify({
+      api: {
+        icon: 'ılıl',
+        name: 'analytics.do',
+        description: 'Simple Analytics Service',
+        url: 'https://analytics.do',
+        api: 'https://analytics.do/api',
+        endpoints: {
+          parse: origin + '/parse?prop=value',
+          generate: origin + '/:url',
+        },
+        type: 'https://apis.do',
+        repo: 'https://github.com/drivly/analytics.do',
+      },
+      user,
+    }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }}) 
+  }
+}
