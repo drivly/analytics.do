fbcf6772f42292f4e0d2f829f42f869e525cb353
Nathan Clevenger <nateclev@gmail.com>
2022-09-03 03:15:44 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,6 +3,7 @@ export default {
     const { user, redirect, body } = await env.CTX.fetch(req).then(res => res.json())
     if (redirect) return Response.redirect(redirect)
     const { origin, pathname, search } = new URL(req.url)
+    env.ANALYTICS.get(env.ANALYTICS.idFromName(new URL(req.url).hostname)).fetch(req)
      
     return new Response(JSON.stringify({
       api: {
@@ -20,5 +21,21 @@ export default {
       },
       user,
     }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }}) 
+  }
+}
+
+export class Analytics {
+  constructor(state, env) {
+    this.state = state
+    this.env = env
+  }
+  async fetch(req) {
+    if (req.url == 'https://analytics.do/api') {
+      const options = Object.fromEntries(new URL(req.url).searchParams)
+      const data = await this.state.storage.list({reverse: true, limit: 1000}).then(list => Object.values(Object.fromEntries(list)))
+      return new Response(JSON.stringify(data))
+    } else {
+      return new Response(JSON.stringify({ stored: true }))
+    }
   }
 }
